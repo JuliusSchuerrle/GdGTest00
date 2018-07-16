5c826e04faeab0495f288277d95aca081f25452e
JuliusSchuerrle <hackehackekuchen@maxap.eu>
2018-07-16 22:58:40 +0200

    commit for processing Lib

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,10 @@
-public class MainClass {
+import processing.core.PApplet;
+
+public class MainClass extends PApplet {
 
     public static void main(String[] args){
+
+        PApplet.main("MainClass");
         System.out.println("hello");
     }
 }
