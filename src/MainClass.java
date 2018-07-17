48baef5e7674b8e69e249f3da4c4020cc77149d0
JuliusSchuerrle <hackehackekuchen@maxap.eu>
2018-07-17 21:58:19 +0200

    added all libs as files and added minim libs to project

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,12 @@
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+import ddf.minim.analysis.FFT;
 import processing.core.PApplet;
 
 public class MainClass extends PApplet {
-
+    Minim minim;
+    AudioPlayer jingle;
+    FFT fft;
     public static void main(String[] args){
 
         PApplet.main("MainClass");
@@ -12,6 +17,12 @@ public class MainClass extends PApplet {
         size(1000,1000);
     }
 
+    public void setup(){
+        minim = new Minim(this);
+        jingle = minim.loadFile("song.mp3", 2048);
+        jingle.loop();
+        fft = new FFT(jingle.bufferSize(), jingle.sampleRate());
+    }
     public void draw(){
         line(0,0,100,100);
     }
