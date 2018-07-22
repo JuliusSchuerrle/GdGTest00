e98abace1f1cda2f1373f9ea526f60a4c0bc3584
Philipp Kochendörfer <philipp.kochendoerfer@outlook.com>
2018-07-22 20:23:02 +0200

    SlideMenu verbessert

--- gui/SlideMenu.java
+++ gui/SlideMenu.java
@@ -3,31 +3,27 @@ import javazoom.jl.decoder.Control;
 import processing.core.PApplet;
 import controlP5.ControlP5;
 import controlP5.Slider;
-
 import java.util.ArrayList;
 import java.util.Dictionary;
 
 public class SlideMenu {
     ControlP5 menu;
-    private final int MINPOS=0;
-    private final int MAXPOS=-1920;
-    public boolean isMoving = false;
-    private int direction = 0;
-    private final int STDLENGTH = 200;
 
-    private int cnt = 0;
-    private int xPos=0;
-    private int yPos=0;
-    private int NxPos=xPos;
-    private int NyPos=yPos;
+    //Settings
+
+    private final int LENGTH = 400;
+    private final int HEIGHT = 100;
+    private final float SPEED  = 0.05f;
+
+    //Settings
+
     private Slider left;
     private Slider middle;
     private Slider right;
 
-
-    private final int MIDPOSX = 1920/2-STDLENGTH/2;
+    private final int MIDPOSX = 1920/2-LENGTH/2;
     private final int MIDPOSY = 1080-150;
-    private final int DISTANCE = 1920/2+STDLENGTH;
+    private final int DISTANCE = 1920/2+LENGTH;
 
     ArrayList<Slider> sliders = new ArrayList<>();
 
@@ -57,36 +53,32 @@ public class SlideMenu {
         right.update();
 
     }
+
+
+
+
+
     public boolean move(){
-        middle.setPosition( middle.getPosition()[0]+ direction*5,middle.getPosition()[1]);
-        left.setPosition( right.getPosition()[0]+ direction*5,right.getPosition()[1]);
-        right.setPosition( right.getPosition()[0]+ direction*5,right.getPosition()[1]);
-        if(cnt >= DISTANCE){
+        float way =  interpolation(i);
+
+        left.setPosition( MIDPOSX-DISTANCE + direction *way,left.getPosition()[1]);
+        middle.setPosition( MIDPOSX + direction*way,middle.getPosition()[1]);
+        right.setPosition( MIDPOSX+DISTANCE+ direction*way,right.getPosition()[1]);
+
+        if(i > 1.0f){
             if(direction>0){
                 right.setVisible(false);
                 right=middle;
                 middle=left;
-
-                /*int i=sliders.indexOf(middle)-1;
-                if(i==-1)
-                    i=sliders.size()-1;
-                left=sliders.get(i);
-                System.out.println((i));
-                */
-
-                //TODO Wenn -1 dann slider.size()-1
-
                 left=sliders.get(((sliders.indexOf(middle)-1)%sliders.size()+sliders.size()) %sliders.size());
-
                 setDisplay();
+
             }else if(direction<0){
                 left.setVisible(false);
                 left=middle;
                 middle=right;
                 right=sliders.get((sliders.indexOf(middle)+1)%sliders.size());
                 setDisplay();
-
-
             }
 
             direction = 0;
@@ -98,35 +90,32 @@ public class SlideMenu {
 
 
         }
-        cnt+=5;
+        i+=SPEED;
         return true;
 
     }
 
     public void right(){
-        cnt=0;
+        i=0;
         if(!isMoving) {
-
-            NxPos = xPos - DISTANCE;
             isMoving = true;
             direction = -1;
         }
     }
     public void left(){
-        cnt=0;
+        i=0;
         if(!isMoving) {
-            NxPos = xPos + DISTANCE;
             isMoving = true;
             direction = 1;
         }
     }
 
     private void setupMenu(){
-        s1=menu.addSlider("mainCircleRotSpeed").setWidth(STDLENGTH).setHeight(50).setVisible(false);
-        s2=menu.addSlider("mainCircleRadius").setWidth(STDLENGTH).setHeight(50).setVisible(false);
-        s3=menu.addSlider("pointRadius").setWidth(STDLENGTH).setHeight(50).setVisible(false);
-        s4=menu.addSlider("smallCircleRadius").setWidth(STDLENGTH).setHeight(50).setVisible(false);
-        s5=menu.addSlider("smallCircleSpeed").setWidth(STDLENGTH).setHeight(50).setVisible(false);
+        s1=menu.addSlider("mainCircleRotSpeed").setWidth(LENGTH).setHeight(HEIGHT).setVisible(false);
+        s2=menu.addSlider("mainCircleRadius").setWidth(LENGTH).setHeight(HEIGHT).setVisible(false);
+        s3=menu.addSlider("pointRadius").setWidth(LENGTH).setHeight(HEIGHT).setVisible(false);
+        s4=menu.addSlider("smallCircleRadius").setWidth(LENGTH).setHeight(HEIGHT).setVisible(false);
+        s5=menu.addSlider("smallCircleSpeed").setWidth(LENGTH).setHeight(HEIGHT).setVisible(false);
 
         sliders.add(s1);
         sliders.add(s2);
@@ -149,7 +138,15 @@ public class SlideMenu {
         middle.setVisible(true);
         right.setVisible(true);
     }
+    private float interpolation(float i){
+        float y = (float) Math.sin(i*Math.PI-0.5f*Math.PI)*(DISTANCE/2f)+(DISTANCE/2f);
+        System.out.println(y);
+        return y;
+    }
 
 
     Slider s1,s2,s3,s4,s5;
+    private float i = 0f;
+    private int direction = 0;
+    public boolean isMoving = false;
 }
